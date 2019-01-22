2e80391a1f685f5d6281d589fc63d77be26e12f1
saudagarsulaiman <saudagar.sulaiman@gmail.com>
2019-01-22 14:45:42 +0530

    VC7 Crash issue fixed Commit

--- app/src/main/java/com/cryptowallet/deviantx/UI/Receiver/RefreshServiceReceiver.java
+++ app/src/main/java/com/cryptowallet/deviantx/UI/Receiver/RefreshServiceReceiver.java
@@ -3,6 +3,7 @@ package com.cryptowallet.deviantx.UI.Receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import com.cryptowallet.deviantx.UI.Services.AirdropWalletFetch;
@@ -15,11 +16,25 @@ public class RefreshServiceReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("Local_cache", "Receiver Load");
-        Intent walletIntent = new Intent(context, WalletDataFetch.class);
-        context.startService(walletIntent);
-        Intent coinsIntent = new Intent(context, AllCoinsFetch.class);
-        context.startService(coinsIntent);
-        Intent airdropIntent = new Intent(context, AirdropWalletFetch.class);
-        context.startService(airdropIntent);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(new Intent(context, WalletDataFetch.class));
+                context.startForegroundService(new Intent(context, AllCoinsFetch.class));
+                context.startForegroundService(new Intent(context, AirdropWalletFetch.class));
+            } else {
+                context.startService(new Intent(context, WalletDataFetch.class));
+                context.startService(new Intent(context, AllCoinsFetch.class));
+                context.startService(new Intent(context, AirdropWalletFetch.class));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        Intent walletIntent = new Intent(context, WalletDataFetch.class);
+//        context.startService(walletIntent);
+//        Intent coinsIntent = new Intent(context, AllCoinsFetch.class);
+//        context.startService(coinsIntent);
+//        Intent airdropIntent = new Intent(context, AirdropWalletFetch.class);
+//        context.startService(airdropIntent);
     }
 }
