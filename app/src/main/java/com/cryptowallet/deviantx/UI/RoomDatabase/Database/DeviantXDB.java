5405e1ba850bbbe0cc3a5fc75e1e8890e50c15bb
saudagarsulaiman <saudagar.sulaiman@gmail.com>
2019-03-14 18:11:57 +0530

    vc10 final Commit

--- app/src/main/java/com/cryptowallet/deviantx/UI/RoomDatabase/Database/DeviantXDB.java
+++ app/src/main/java/com/cryptowallet/deviantx/UI/RoomDatabase/Database/DeviantXDB.java
@@ -33,7 +33,7 @@ import com.cryptowallet.deviantx.UI.RoomDatabase.ModelsRoomDB.WalletDetailsDB;
 import com.cryptowallet.deviantx.UI.RoomDatabase.ModelsRoomDB.WalletListDB;
 
 
-@Database(entities = {WalletListDB.class, AccountWalletDB.class, AllCoinsDB.class, AirdropWalletDB.class, ExploreCoinsDB.class, FeaturedAirdropsDB.class, DividendAirdropsDB.class, AirdropsHistoryDB.class, NewsDXDB.class, HeaderBannerDB.class, WalletDetailsDB.class, PairsListDB.class, ExcOrdersDB.class}, version = 2)
+@Database(entities = {WalletListDB.class, AccountWalletDB.class, AllCoinsDB.class, AirdropWalletDB.class, ExploreCoinsDB.class, FeaturedAirdropsDB.class, DividendAirdropsDB.class, AirdropsHistoryDB.class, NewsDXDB.class, HeaderBannerDB.class, WalletDetailsDB.class, PairsListDB.class, ExcOrdersDB.class}, version = 1)
 public abstract class DeviantXDB extends RoomDatabase {
 
     public abstract WalletListDao walletListDao();
@@ -72,11 +72,22 @@ public abstract class DeviantXDB extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             DeviantXDB.class, "deviantx_database")
                             .allowMainThreadQueries()
+//                            .addMigrations(MIGRATION_1_2)
+//                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+/*
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+
+        }
+    };
+*/
 
 }
